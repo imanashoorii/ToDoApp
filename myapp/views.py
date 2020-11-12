4509625f97c545b68acb3f19368d73539e2179f0
Iman Ashoori <56173335+imanashoorii@users.noreply.github.com>
2020-11-12 23:35:12 +0330

    connect and grab from db

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from . import models
 
 
 # Create your views here.
 
 def home(request):
+    return render(request, 'main/index.html')
+
+
+def add_todo(request):
+    print(request.POST)
+
+    content = request.POST.get('content')
+    models.Todo.objects.create(text=content)
+    print(content)
     return render(request, 'main/index.html')
